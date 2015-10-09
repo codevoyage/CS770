a164045c3fc469b19db7d8bacf3140e55095623b
Shikha Mahajan <s7mahaja@scslt155.cs.uwaterloo.ca>
2015-10-09 15:48:21 -0400

    Corrected algorithm

--- Assignment 3/eigen_roots.py
+++ Assignment 3/eigen_roots.py
@@ -2,14 +2,20 @@
 
 import numpy as np
 
-a = np.array([30.,7,-90,45,20,-12])
+a = np.array([30.,7.,-90.,45.,20.,-12.])
+a = -1*a/a[0]
+a = np.delete(a,0)
+
+#print a
+
 n = len(a)
 
 #Generate Matrix to pass to the function
 A = [[1 if i==j else 0 for i in range (0,n)] for j in range(0,n-1)]
 A.insert(0,a)
 
-#Compute Eigen Value of the matrix
+#print A
+#Compute Eigen V
 v,w = np.linalg.eig(A)
 
 print "The roots of the equation are: "+ str(v)
