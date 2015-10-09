8ba205f09960a90e9a10fcd6781f3d41649667e5
Shikha Mahajan <s7mahaja@scslt155.cs.uwaterloo.ca>
2015-10-08 21:59:01 -0400

    eigen roots

---
+++ Assignment 3/eigen_roots.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+a = np.array([30.,7,-90,45,20,-12])
+n = len(a)
+
+#Generate Matrix to pass to the function
+A = [[1 if i==j else 0 for i in range (0,n)] for j in range(0,n-1)]
+A.insert(0,a)
+
+#Compute Eigen Value of the matrix
+v,w = np.linalg.eig(A)
+
+print "The roots of the equation are: "+ str(v)
