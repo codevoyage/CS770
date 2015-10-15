2a9f7f9db4be97d71eb61c2c18fbf76bbc024bfa
Shikha Mahajan <s7mahaja@scslt155.cs.uwaterloo.ca>
2015-10-15 14:53:33 -0400

    Working newtons method

--- Assignment 3/run_Newtons_method.py
+++ Assignment 3/run_Newtons_method.py
@@ -1,56 +1,43 @@
-
-# coding: utf-8
-
-# # run_Newtons_method
-
-# In[1]:
-
 import numpy as np
 
-
 # ## (b) VectorNewtonsMethod
-
-# In[24]:
-
 def VectorNewtonsMethod(fcn, Jacobian, x_orig, xtol):
     for i in range (0,21):
-        x_change = np.linalg.solve(Jacobian(x_orig), -1*fcn(x_orig))
-        if x_change > xtol:
+        x_change = np.linalg.solve(Jacobian(x_orig), fcn(x_orig))
+        if x_change.any() > xtol:
             x_orig+=x_change
         else:
             break
     if i>20 or i==20:
-        print "FAILED"
+        return "FAILED"
         
     return x_orig
 
 # ## (c) Solve the given system of equations
-# ### First, create the needed functions.
-
-# In[16]:
-#print e
 # The function to find the roots of
 def my_f(x):
-    x1 =np.array([])
+    x1 =[]
     x1.append( x[0]**2 + x[1]**2 -x[2]-1)
     x1.append( x[0]+ x[1]*x[2])
     x1.append(e**x[0]-1.0/2.0)
+    x1 = np.array(x1)*-1
     return x1
 
 # The function's Jacobian
 def my_Jacobian(x):
-    x1 =np.append([[2*x[0],2*x[1],-1],axis=0)
-    x1 =np.append([1,x[2],x[1]],0)
-    x1 =np.append([e**x[0],0,0],0)
+    x1=[]
+    x1.append([2*x[0],2*x[1],-1])
+    x1.append([1,x[2],x[1]])
+    x1.append([e**x[0],0,0])
     return x1
-#my_Jacobian([1,2,3])
 
-# ### Then call your VectorNewtonsMethod function
-# (using a couple different initial guesses)
 xtol = 10**-7
-x_orig =np.array( [0,1,0])
-VectorNewtonsMethod(my_f, my_Jacobian, x_orig, xtol )
-# In[ ]:
+x_orig = [0,1,0]
+print VectorNewtonsMethod(my_f, my_Jacobian, x_orig, xtol )
+x_orig = [0.4,0,0.9]
+print VectorNewtonsMethod(my_f, my_Jacobian, x_orig, xtol )
+
+
 
 
 
