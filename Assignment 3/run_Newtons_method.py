1dad6222d67796e9e53d14e60e6038807bd367f7
Shikha Mahajan <s7mahaja@scslt155.cs.uwaterloo.ca>
2015-10-15 22:28:59 -0400

    all stopping criteria

--- Assignment 3/run_Newtons_method.py
+++ Assignment 3/run_Newtons_method.py
@@ -4,13 +4,14 @@ import numpy as np
 def VectorNewtonsMethod(fcn, Jacobian, x_orig, xtol):
     for i in range (0,21):
         x_change = np.linalg.solve(Jacobian(x_orig), fcn(x_orig))
-        if x_change.any() > xtol:
+        if x_change.all() > xtol:
             x_orig+=x_change
         else:
+            print("i ",i)
             break
     if i>20 or i==20:
         return "FAILED"
-        
+     
     return x_orig
 
 # ## (c) Solve the given system of equations
@@ -22,7 +23,7 @@ def my_f(x):
     x1.append(e**x[0]-1.0/2.0)
     x1 = np.array(x1)*-1
     return x1
-
+#print my_f([-0.69314718,321,234])
 # The function's Jacobian
 def my_Jacobian(x):
     x1=[]
@@ -31,10 +32,10 @@ def my_Jacobian(x):
     x1.append([e**x[0],0,0])
     return x1
 
-xtol = 10**-7
-x_orig = [0,1,0]
+xtol = 0.0000001
+x_orig = [0.0,1.0,0.0]
 print VectorNewtonsMethod(my_f, my_Jacobian, x_orig, xtol )
-x_orig = [0.4,0,0.9]
+x_orig = [0.4,0.0,0.9]
 print VectorNewtonsMethod(my_f, my_Jacobian, x_orig, xtol )
 
 
